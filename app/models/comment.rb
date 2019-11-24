e08eb8a38d7bda31c104691985d30fb777924a5d
Vincent Garrigues <vincent.garrigues@gmail.com>
2019-11-24 21:05:50 +0100

    Add comment validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :proposal
+
+  validates :body, presence: true, length: { minimum: 20 }
 end
--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,4 +1,6 @@
 class Proposal < ApplicationRecord
+  has_many :comments
+
   validates :title, presence: true, length: { minimum: 5 }
   validates :description, presence: true, length: { minimum: 20 }
 end
