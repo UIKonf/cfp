1830faeaf1253c1fae12cb010d9c4674edcae934
Vincent Garrigues <vincent.garrigues@gmail.com>
2019-11-26 00:29:08 +0100

    User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_195021) do
+ActiveRecord::Schema.define(version: 2019_11_25_231308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,16 @@ ActiveRecord::Schema.define(version: 2019_11_24_195021) do
   create_table "proposals", force: :cascade do |t|
     t.string "title"
     t.string "description"
-    t.boolean "confirmed", default: false
-    t.boolean "withdrawn", default: false
+    t.boolean "live", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "github_uid"
+    t.string "github_nickname"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
