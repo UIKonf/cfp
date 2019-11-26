f90117a51c12de26a921dd16aac2aed56875f4f8
Vincent Garrigues <vincent.garrigues@gmail.com>
2019-11-26 20:33:17 +0100

    login, logout w/ dropdown

--- app/controllers/github_authentication_controller.rb
+++ app/controllers/github_authentication_controller.rb
@@ -4,16 +4,17 @@ class GithubAuthenticationController < ApplicationController
     @user = User.find_by_github_uid(auth_hash[:uid]) || User.create_with_omniauth(auth_hash)
 
     if @user
-      session[:user_id] = @user.id
+      log_in(@user)
       flash[:success] = 'Logged in successfully'
       redirect_to user_url(@user)
     else
+      flash[:danger] = 'Ooops, something went wrong logging you in :/'
       redirect_to login_url
     end
   end
 
   def logout
-    session[:user_id] = nil
+    log_out
     flash[:success] = 'Logged out'
     redirect_to root_url
   end
