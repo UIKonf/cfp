26bee6cac4ba34c1cd5a4cbf100600b2742f27f7
Vincent Garrigues <vincent.garrigues@gmail.com>
2019-11-27 19:25:04 +0100

    Flash a warning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user!
     unless logged_in?
-      flash[:alert] = 'You need to log in to see this.'
+      flash[:warning] = 'You need to log in to see this.'
       redirect_to login_url
     end
   end
