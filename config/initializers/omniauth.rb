7a20e2b3c7cc234b3190d86da73da0b13c9cff60
Vincent Garrigues <vincent.garrigues@gmail.com>
2019-11-27 19:10:31 +0100

    Silence omniauth logging

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'user:email'
 end
+
+OmniAuth.config.logger = Rails.logger
