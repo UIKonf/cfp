c50a81cb3dd8d75be7578ede0f4c85ae20c2a422
Vincent Garrigues <vincent.garrigues@gmail.com>
2019-11-27 19:55:15 +0100

    Generate fake proposals

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+99.times do |_n|
+  title = Faker::Hipster.sentence(word_count: 3)
+  description = Faker::Markdown.sandwich(sentences: 5)
+  Proposal.create!(title: title, description: description)
+end
