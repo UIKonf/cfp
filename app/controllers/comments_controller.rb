aeabd05531a14f26f788df7a53e1f3e42bfe9902
Vincent Garrigues <vincent.garrigues@gmail.com>
2019-12-12 18:14:40 +0100

    comments controller tests

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,17 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @proposal = Proposal.find(params[:proposal_id])
-    @comment = @proposal.comments.create(comment_params)
+    @comment = current_user.comments.create(comment_params.merge(proposal: @proposal))
     @comment.save
     redirect_to proposal_path(@proposal)
   end
 
   def destroy
     @proposal = Proposal.find(params[:proposal_id])
-    @comment = @proposal.comments.find(params[:id])
-    @comment.destroy
+    @comment = current_user.comments.find_by_id(params[:id])
+    @comment&.destroy
     redirect_to proposal_path(@proposal)
   end
 
--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    @proposal = proposals(:one)
+  end
+
+  test 'only accessible when logged in' do
+    post proposal_comments_path(@proposal)
+    assert_authenticated_only
+  end
+
+  test 'should create' do
+    log_in_as(@user)
+    assert_difference 'Comment.count', 1 do
+      post proposal_comments_path(@proposal), params: {comment: {body: 'b' * 200}}
+    end
+    assert_redirected_to @proposal
+  end
+
+  test 'should delete' do
+    log_in_as(@user)
+    comment = @user.comments.create(body: 'b' * 200, proposal: @proposal)
+    delete proposal_comment_path(@proposal, comment)
+    assert_redirected_to @proposal
+  end
+
+  test 'should only delete comments the logged in user posted' do
+    log_in_as(users(:two))
+    comment = @user.comments.create(body: 'b' * 200, proposal: @proposal)
+    assert_difference 'Comment.count', 0 do
+      delete proposal_comment_path(@proposal, comment)
+    end
+    assert_redirected_to @proposal
+  end
 end
