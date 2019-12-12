8b3a7019756a904101f9162c613f0a5f18045314
Vincent Garrigues <vincent.garrigues@gmail.com>
2019-12-12 17:50:12 +0100

    render markdown description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,14 @@ module ApplicationHelper
       page_title + ' | ' + base_title
     end
   end
+
+  def markdown(text)
+    markdown_renderer.render(text).html_safe
+  end
+
+  private
+
+  def markdown_renderer
+    @markdown_rendered ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, safe_links_only: true, escape_html: true, prettify: true)
+  end
 end
