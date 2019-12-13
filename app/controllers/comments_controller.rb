e7e5d4afcce9c7bd7002e13fdff49def12731026
Vincent Garrigues <vincent.garrigues@gmail.com>
2019-12-13 09:57:29 +0100

    Nicer comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
     @proposal = Proposal.find(params[:proposal_id])
     @comment = current_user.comments.create(comment_params.merge(proposal: @proposal))
-    @comment.save
+    flash[:danger] = 'Your comment could not be saved, maybe it is too short?' unless @comment.save
     redirect_to proposal_path(@proposal)
   end
 
