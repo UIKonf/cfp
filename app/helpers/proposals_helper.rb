96079f0b1faaaa19f54fa01223eb868e3466b5b9
Vincent Garrigues <vincent@garriguv.io>
2019-12-27 18:11:28 +0100

    Refactor proposal buttons

--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -3,13 +3,19 @@ module ProposalsHelper
     proposal.published? || proposal.preselected?
   end
 
-  def edit_button
+  def create_proposal_button
+    if can?(:create, :proposal)
+      link_to 'New Proposal', new_proposal_path, class: 'btn btn-primary'
+    end
+  end
+
+  def edit_proposal_button
     if can?(:update, :proposal)
       link_to 'Edit', edit_proposal_path(@proposal), class: 'btn btn-primary'
     end
   end
 
-  def publish_or_withdraw_button
+  def publish_or_withdraw_proposal_button
     if @proposal.can_withdraw? && can?(:withdraw, :proposal)
       link_to 'Withdraw', withdraw_proposal_path(@proposal), method: :post, class: 'btn btn-warning', data: {confirm: 'Are you sure?'}
     elsif @proposal.can_publish? && can?(:publish, :proposal)
@@ -17,7 +23,7 @@ module ProposalsHelper
     end
   end
 
-  def delete_button
+  def delete_proposal_button
     if can?(:destroy, :proposal)
       link_to 'Delete Forever', proposal_path(@proposal), method: :delete, class: 'btn btn-danger', data: {confirm: 'Are you sure?'}
     end
