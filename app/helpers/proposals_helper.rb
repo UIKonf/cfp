9ddca27e51573980b021fdd4cf21a9401a414545
Vincent Garrigues <vincent@garriguv.io>
2019-12-27 18:05:07 +0100

    Add buttons to proposals helper

--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -2,4 +2,24 @@ module ProposalsHelper
   def show_comments?(proposal)
     proposal.published? || proposal.preselected?
   end
+
+  def edit_button
+    if can?(:update, :proposal)
+      link_to 'Edit', edit_proposal_path(@proposal), class: 'btn btn-primary'
+    end
+  end
+
+  def publish_or_withdraw_button
+    if @proposal.can_withdraw? && can?(:withdraw, :proposal)
+      link_to 'Withdraw', withdraw_proposal_path(@proposal), method: :post, class: 'btn btn-warning', data: {confirm: 'Are you sure?'}
+    elsif @proposal.can_publish? && can?(:publish, :proposal)
+      link_to 'Publish', publish_proposal_path(@proposal), method: :post, class: 'btn btn-success', data: {confirm: 'Are you sure?'}
+    end
+  end
+
+  def delete_button
+    if can?(:destroy, :proposal)
+      link_to 'Delete Forever', proposal_path(@proposal), method: :delete, class: 'btn btn-danger', data: {confirm: 'Are you sure?'}
+    end
+  end
 end
